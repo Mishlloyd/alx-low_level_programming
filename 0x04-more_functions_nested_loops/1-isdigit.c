b351493c54ac94a8fe3df09f5236f734187ee1bd
mishlloyd <mishuabate@gmail.com>
2023-07-20 12:54:07 -0700

    second time

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * _islower - check for lowercase
+ * _isdigit - check for lowercase
  * @c: The character in ASCII code
  * Return: 1 for lowercase character. 0 for the rest.
  */
 int _isdigit(int c)
 {
-        if (c >= '0' && c <= '9')
+	if (c >= '0' && c <= '9')
 		return (1);
 	else
 		return (0);
