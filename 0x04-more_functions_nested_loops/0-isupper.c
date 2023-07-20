9307fa2cff2f1d241a8b22e795cc0192ee4449e9
mishlloyd <mishuabate@gmail.com>
2023-07-20 10:06:48 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,9 +8,13 @@
  */
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
-		_putchar (1);
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
 	else
-		_putchar (0)
-	return (0);
+	{
+		return (0);
+	}
+	_putchar('\n');
 }
