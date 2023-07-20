9b2169c1c895304a65b71872417617517accaa13
mishlloyd <mishuabate@gmail.com>
2023-07-20 12:34:37 -0700

    two in one

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - check for lowercase
+ * @c: The character in ASCII code
+ * Return: 1 for lowercase character. 0 for the rest.
+ */
+int _isdigit(int c)
+{
+        if (c >= 48 && c <= 57)
+        {
+                return (1);
+        }
+        else
+        {
+                return (0);
+        }
+}
